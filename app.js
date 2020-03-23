3e95e1c4dc283b5d828cbf3b1c20a04179b197e8
Forrest Miller <Forrestmillerj@gmail.com>
2020-03-22 21:43:18 -0500

    Created function to write out title of project using figlet.

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log("hello")
+const DB = require("./data/connection");
+const figlet = require("figlet")
+
+function writeText () {
+    figlet('Employee Tracker!!', {
+        horizontalLayout: 'full',
+    },function(err, data) {
+        if (err) {
+            console.log('Something went wrong...');
+            console.dir(err);
+            return;
+        }
+        console.log(data)
+    });
+};
+
+writeText();
+// DB.createEmployee(850, "Johnny", "Bravo", 8, 202);
+// DB.createDepartment(8,"Europe");
+// DB.createRole(8,"Janitor", 25000.00, 8)
+// DB.deleteEmployee("Johnny");
+
+// DB.updateRole(4, "Johnny");
+
+// DB.readDepartment();
+// DB.readRole();
+// DB.readEmployee();
+
+// console.log(DB);
