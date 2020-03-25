0d4ef9490bc6a3d033882dcdb55665bae67713bd
Forrest Miller <Forrestmillerj@gmail.com>
2020-03-24 22:47:31 -0500

    updated Schema

--- data/companySCHEMA.sql
+++ data/companySCHEMA.sql
@@ -4,13 +4,13 @@ CREATE DATABASE company_db;
 USE company_db;
 
 CREATE TABLE department (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY NOT NULL,
     name VARCHAR(30) NOT NULL
 );
 
 
 CREATE TABLE role (
-    id INT PRIMARY KEY NOT NULL,
+    id INT(10) AUTO_INCREMENT PRIMARY KEY NOT NULL,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10,2),
     department_id INT,
@@ -18,11 +18,12 @@ CREATE TABLE role (
 );
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY,
+    id INT(10) AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
+    role_title VARCHAR(30) NULL,
     role_id INT,
-    manager_id INT
+    manager_id INT 
 );
 
 -- Set employee.role_id to reference role.id
